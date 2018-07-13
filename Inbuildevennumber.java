ec8f1dce624ef2f65c38958511aeb280cf00f1b2
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-13 21:38:11 +0530

    Inbuid even digit

---
+++ Inbuildevennumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Inbuildevennumber {
+public static void main(String[]args){
+	int a,b,i;
+	Scanner sc=new Scanner(System.in);
+	a=sc.nextInt();
+	b=sc.nextInt();
+	for(i=a+1;i<b;i++){
+		if(i%2==0){
+			System.out.println(i);
+		}
+	}
+	
+}
+}

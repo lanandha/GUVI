ad7d9277fd4f3413554dbd1b366222481d922974
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-13 18:57:45 +0530

    Square

---
+++ Square.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Square {
+public static void main(String[]args){
+	int i,j,a,b,c=1;
+	Scanner sc=new Scanner(System.in);
+	a=sc.nextInt();
+	b=sc.nextInt();
+	for(i=1;i<=b;i++){
+		for(j=a;j<a;j++){
+			c=c*j;
+			
+		}
+		
+	}System.out.println(c);
+}
+}

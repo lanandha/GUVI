df030ce00a8530618cad0a7852642f155e40357c
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-13 18:34:57 +0530

    Integer count

---
+++ Count.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Count {
+public static void main(String[]args){
+	int n;
+	Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	System.out.println(String.valueOf(n).length());
+}
+}

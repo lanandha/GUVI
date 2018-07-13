0743efa7459b3bdccd572ce5c2c59e92c077d9ad
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-13 17:53:00 +0530

    multiple times repeat

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Hello {
+public static void main(String[]args){
+	String s;
+	int a,i;
+	Scanner sc= new Scanner(System.in);
+	//s=sc.nextLine();
+	a=sc.nextInt();
+	s=sc.nextLine();
+	for(i=0;i<a;i++){
+		
+		System.out.println(s);
+	}
+}
+}

ee5c6e42efef84113423c9b1cd828f0cab58f5d8
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-14 18:39:24 +0530

    Add to number

---
+++ Arthimetric.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.*;
+public class Arthimetric {
+public static void main(String[]args){
+	int a,fact=0;
+	Scanner sc=new Scanner(System.in);
+	a=sc.nextInt();
+	for(int i=1;i<=5;i++){
+		fact=fact+a;
+	System.out.print(fact+" ");
+}}
+}

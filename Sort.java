4b851a552536700f324d5ce0bb0702715556412e
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-15 10:30:41 +0530

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Sort {
+public static void main(String[]args){
+		int i,b,n;
+		Scanner in=new Scanner(System.in);
+		b=in.nextInt();
+		int a[]=new int[b];
+		for( i=0;i<b;i++){
+			a[i]=in.nextInt();
+		}Arrays.sort(a);
+		for( i=0;i<b;i++){
+			System.out.print(a[i]+" ");
+		}
+}
+}

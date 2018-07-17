93bad001e6a776f3c65dc71d8cd55bb58afb196a
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-17 14:10:38 +0530

    position

---
+++ Position/src/Position.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Position {
+public static void main(String[]args){
+	int i,n;
+	Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	int a[]=new int[n];
+	for(i=0;i<n;i++){
+		a[i]=sc.nextInt();}
+	for(i=0;i<n;i++){
+	System.out.println(a[i]+" "+i);
+}}
+}

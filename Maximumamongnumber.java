cf5a0cc4dd578f9e66195f4a3b6f06b742dd8611
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-19 19:15:40 +0530

    Add files via upload
    maximum among a number

---
+++ Maximumamongnumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+//import java.util.Arrays;
+public class Maximumamongnumber {
+public static void main(String[]args){
+	int i;
+	Scanner sc=new Scanner(System.in);
+	int a[]=new int[10];
+	for( i=0;i<10;i++){
+		a[i]=sc.nextInt();
+	}
+	Arrays.sort(a);
+	//for(i=0;i<10;i++){
+		System.out.println(a[9]);
+
+}
+}

8c09afbb2f4a56a158f22ac4de356e9681035b47
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-19 18:20:42 +0530

    space

---
+++ Space.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Space {
+public static void main(String[]args){
+	String s;
+	Scanner sc= new Scanner(System.in);
+	s=sc.nextLine();
+	String srr[]=s.split(" ");
+	int len=srr.length-1;
+	System.out.println(len);
+}
+}

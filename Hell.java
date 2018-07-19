6ed29ae73170236bf00e07199df27b9e1fbbc7ab
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-19 18:21:06 +0530

    Hello world

---
+++ Hell.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hell {
+public static void main(String[]args){
+	String s;
+	Scanner sc=new Scanner(System.in);
+	s=sc.nextLine();
+	String sr[]=s.split(" ");
+	int len=sr.length;
+	System.out.println(len);
+}
+}

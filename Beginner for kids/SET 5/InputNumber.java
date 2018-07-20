895ebc47e9839c2f030ca5fc4e730af30e90fd18
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 10:45:55 +0530

    Add files via upload
    46 - Given a number N, print the next number.
    Input Size : |N| <= 10000000
    Sample Testcase :
    INPUT
    123
    OUTPUT
    124   variables operators conditional looping

---
+++ Beginner for kids/SET 5/InputNumber.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class InputNumber {
+ public static void main(String[]args){
+	 int a,b;
+	 Scanner sc=new Scanner(System.in);
+	a=sc.nextInt();
+	b=a+1;
+	System.out.println(b);
+ }
+}

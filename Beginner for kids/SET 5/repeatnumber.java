7cd007de8cb9a6620e5d932f42c4b86ff13bbf93
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 09:24:51 +0530

    Add files via upload
    Repeatednumber

---
+++ Beginner for kids/SET 5/repeatnumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class repeatnumber {
+public static void main(String[]args){
+	String s;
+	int b,i;
+	Scanner sc=new Scanner(System.in);
+	s=sc.nextLine();
+	String a[]=s.split(" ");
+    b=Integer.valueOf(a[1]);
+	for(i=0;i<b;i++){
+		System.out.println(a[0]);
+	}
+	//int arr[]=new int[];
+	
+}
+}

bf2020ddd0b9807db817327a232b6671afda17c4
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 11:59:28 +0530

    Add files via upload
    My-Submission : Not Submitted
    52 - Given a number N, print it in words.
    Input Size : 1 <= N <= 10
    Sample Testcase :
    INPUT
    1
    OUTPUT
    One - (Posted by Poornima TM)   variables operators conditional Math Strings

---
+++ Beginner for kids/SET 6/Switch.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class Switch {
+ public static void main(String[]args){
+	 int a;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	 switch(a){
+	 case 1:
+	 {
+		 System.out.println("one");
+		 break;
+	 }
+	 case 2:
+	 {
+		 System.out.println("Two");
+		 break;
+	 }
+	 case 3:
+	 {
+		 System.out.println("Three");
+		 break;
+	 }
+	 case 4:
+	 {
+		 System.out.println("Four");
+		 break;
+	 }
+	 case 5:
+	 {
+		 System.out.println("five");
+		 break;
+	 }
+	 case 6:
+	 {
+		 System.out.println("six");
+		 break;
+	 }
+	 case 7:
+	 {
+		 System.out.println("seven");
+		 break;
+	 }
+	 case 8:
+	 {
+		 System.out.println("Eight");
+		 break;
+	 }
+	 case 9:
+	 {
+		 System.out.println("Nine");
+		 break;
+	 }
+	 }
+ }
+}

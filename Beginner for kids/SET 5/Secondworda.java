691d77c67be6ab68e3deb2173c8b59348156fe2e
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 09:34:42 +0530

    Add files via upload
    Space not allowed

---
+++ Beginner for kids/SET 5/Secondworda.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Secondworda {
+public static void main(String[]args){
+	String s;
+	Scanner sc=new Scanner(System.in);
+	s=sc.nextLine();
+	String ss=s.replaceAll(" ","");
+	System.out.println(ss);
+}
+}

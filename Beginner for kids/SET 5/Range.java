0c42180d826546f286468179b0eed8165909649a
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 10:21:00 +0530

    Add files via upload
    Range inbetween

---
+++ Beginner for kids/SET 5/Range.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Range {
+ public static void main(String[]args){
+	 int i,a=1,b=10,c;
+	 Scanner sc=new Scanner(System.in);
+c=sc.nextInt();
+if(b>c){
+	if(c>a){
+		 System.out.println("yes");
+	 }else{
+		 System.out.println("no");
+	 }
+ }else{
+ System.out.println("no");
+ }
+ }
+ }

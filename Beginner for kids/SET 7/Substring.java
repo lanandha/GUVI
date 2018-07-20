cbebba83817b58768ccfeb87f7b6eed71899d56f
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 17:41:26 +0530

    Add files via upload
    How to submit answer in codekata?(Video tutorial)
    61 - Given a string S and a number K, print the first K characters.
    Sample Testcase :
    INPUT
    laptop 3
    OUTPUT
    lap

---
+++ Beginner for kids/SET 7/Substring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Substring {
+ public static void main(String[]args){
+	 String s,a;
+	 Scanner sc=new Scanner(System.in);
+	 s=sc.nextLine();
+	 a=s.substring(0,3);
+	 System.out.println(a);
+ }
+}

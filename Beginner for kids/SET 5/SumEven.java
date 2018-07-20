8dd7af000bc3c02be9da289b32e1f4efcc04f963
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 11:30:35 +0530

    Add files via upload
    50 - Given a number N find whether it is a power of 2.
    Sample Testcase :
    INPUT
    4
    OUTPUT
    yes - (Posted by TB Nanda)   variables operators conditional Math

---
+++ Beginner for kids/SET 5/SumEven.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class SumEven {
+	public static void main(String[]args){
+		 int a,i,rem,sum=0;
+		 Scanner sc=new Scanner(System.in);
+		 a=sc.nextInt();
+		if(a%2==0){
+		 System.out.println("yes");
+	}else{
+		System.out.println("no");
+	}
+}
+}

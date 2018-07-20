9ca43d29310f484bd3e3aa212a3b9fafb81d5289
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 11:24:27 +0530

    Add files via upload
    49 - Given 2 numbers find their sum.
    Sample Testcase :
    INPUT
    5 5
    OUTPUT
    10 - (Posted by Arun prakash)   variables operators conditional arrays Math

---
+++ Beginner for kids/SET 5/Sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sum {
+	public static void main(String[]args){
+		 int a,i,rem,sum=0;
+		 Scanner sc=new Scanner(System.in);
+		 a=sc.nextInt();
+		 i=sc.nextInt();
+		 rem=a+i;
+		 sum=Math.abs(rem);
+		 System.out.println(sum);
+	}
+}

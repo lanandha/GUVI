0e5478618a3da8f1e7c14b03b34d8d1be9fa8c0b
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 14:27:47 +0530

    Add files via upload
    55 - Given 2 numbers N,M. Print whether the product of two numbers is even or odd.
    Sample Testcase :
    INPUT
    12 3

---
+++ Beginner for kids/SET 6/Evenodd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Evenodd {
+ public static void main(String[]args){
+	 int a ,b;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	 b=sc.nextInt();
+	 if(a%2==0){
+		 System.out.println("Even");
+	 }else{
+		 System.out.println("odd");
+	 }
+ }
+}

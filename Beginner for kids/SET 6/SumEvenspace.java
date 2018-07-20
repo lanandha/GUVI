e28d1cc8a26a6f2d7d58f77e29b88080668ff80c
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 11:51:49 +0530

    Add files via upload
    How to submit answer in codekata?(Video tutorial)
    51 - Given a number N, print all its digits(in same order).
    Sample Testcase :
    INPUT
    123
    OUTPUT
    1 2 3 - (Posted by Nivedha)   variables operators conditional Math pointers

---
+++ Beginner for kids/SET 6/SumEvenspace.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SumEvenspace {
+	public static void main(String[]args){
+		int i;
+		int arr[]=new int[10];
+		Scanner sc=new Scanner(System.in);
+		for(i=0;i<3;i++){
+			arr[i]=sc.nextInt();
+		}
+		for(i=0;i<3;i++){
+			System.out.print(arr[i]+" ");
+		}
+}
+}

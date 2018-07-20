ec3482f30f84b1edf8b9acc59d99e1ca637cd18e
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 09:31:26 +0530

    Add files via upload
    Second letter print

---
+++ Beginner for kids/SET 5/Secondword.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Secondword {
+public static void main(String[]args){
+	String s;
+	Scanner sc=new Scanner(System.in);
+	s=sc.nextLine();
+	String ss[]=s.split(" ");
+	System.out.println(ss[1]);
+}
+}

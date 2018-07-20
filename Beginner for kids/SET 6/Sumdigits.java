2bed6f232a47e1647cd14b965dce895884e5a540
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-20 12:31:27 +0530

    Add files via upload
    54 - Given a number N, print the nearest even number which is less than or equal to N.
    Sample Testcase :
    INPUT
    123
    OUTPUT
    122 - (Posted by Ragu)   variables operators conditional substring

---
+++ Beginner for kids/SET 6/Sumdigits.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sumdigits {
+ public static void main(String[]args){
+	int a,b,c,sum=0;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	 b=a-1;
+	c=Math.abs(b);
+	 
+	 System.out.println(c);
+ }
+}

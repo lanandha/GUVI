6d1b62a8cb69d320f979bb9b35ff769ea2ddc4f9
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-21 14:24:55 +0530

    Add files via upload
    65 - Given a string S, print it without using semicolon in your program.
    Sample Testcase :
    INPUT
    hello world
    OUTPUT
    hello world

---
+++ Beginner for kids/SET 7/Semicolon.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Semicolon {
+ public static void main(String[]args){
+	 String a;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextLine();
+	 if(System.out.printf(a)==null){
+		
+	 }
+ }
+}

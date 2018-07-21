4498907acbfd647fc48a9b575f1b1be557221547
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-21 17:26:48 +0530

    Add files via upload
    70 - Given a number N, find whether its nearest greater power of 2.
    Input Size : N <= 1000
    Sample Testcase :
    INPUT
    4
    OUTPUT
    8

---
+++ Beginner for kids/SET 7/Greaterpower.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Greaterpower {
+	 public static void main(String[]args){
+		 int a,i,b;
+		 Scanner sc= new Scanner(System.in);
+				a=sc.nextInt();
+				i=sc.nextInt();
+				b=a*i;
+		
+			System.out.println(b);
+				//System.out.println("odd");
+		
+		 }
+		 }

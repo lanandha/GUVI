fe4a54e750223e39c4d2969f3656113d19f41781
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-21 17:05:47 +0530

    Add files via upload
    67 - Given a number N, find a nearest greater multiple of 10.
    Input Size : n <= 10000
    Sample Testcase :
    INPUT
    3
    OUTPUT
    10

---
+++ Beginner for kids/SET 7/Nearly.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Nearly {
+ public static void main(String[]args){
+	 int a;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	if(a<10){
+		System.out.println("10");
+	}else if(a<20){
+		System.out.println("20");
+	}else if(a<30){
+		System.out.println("30");
+	}
+	else if(a<40){
+		System.out.println("40");
+	}else if(a<50){
+		System.out.println("50");
+	}
+ }
+}

09ab9d75490daf10276a98b98b6ce3542e0bde30
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-21 17:23:43 +0530

    Add files via upload
    69 - Given 2 numbers N,M. Find their difference and check whether it is even or odd.
    Sample Testcase :
    INPUT
    5 5
    OUTPUT
    even

---
+++ Beginner for kids/SET 7/Evenoroddq.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Evenoroddq {
+	 public static void main(String[]args){
+		 int a,i,b;
+		 Scanner sc= new Scanner(System.in);
+				a=sc.nextInt();
+				i=sc.nextInt();
+				b=a-i;
+		if(b%2==0){
+			System.out.println("Even");
+		}else{
+			System.out.println("odd");
+		}
+		 }
+		 }

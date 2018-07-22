70f09cf9f47fc5d65fcba8edaeaf52ff3b4199bb
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-22 11:59:57 +0530

    74 - Given a decimal number(only 1 decimal place) round it off to nearest greater integer and print it. Sample Testcase : INPUT 2.6 OUTPUT 3
    74 - Given a decimal number(only 1 decimal place) round it off to nearest greater integer and print it.
    Sample Testcase :
    INPUT
    2.6
    OUTPUT
    3

---
+++ Beginner for kids/SET 8/Greatestnumr.java
@@ -0,0 +1,11 @@
+import java.util.*;
+	
+public class Greatestnumr {
+	
+	     public static void main(String aa[]){
+	      float a;
+	      Scanner sc=new Scanner(System.in);
+	      a=sc.nextFloat();
+	      System.out.println((int)a+1);
+	}
+}

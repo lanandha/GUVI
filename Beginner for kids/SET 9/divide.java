1fe0c3c429b34df0d00bd01a0f091591dd045c12
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-23 12:21:24 +0530

    83 - "ooooooooooooooh! If I could do the easy mathematics like my school days!! I can guarantee, that I'd not make any mistake this time!!" Says a smart university student!! But his teacher even smarter "Ok! I'd assign you such projects in your software lab. Don't be so sad." "Really!!" the  students  feels  happy. And  he  feels  so happy that he cannot see the smile in his teacher's face. The  rst project for the poor student was to make a calculator that can just perform the basic arithmetic operations. But like many other university students he doesn't like to do any project by himself. He just wants to collect programs from here and there. As you are a friend of him, he asks you to write the program. But, you are also intelligent enough to tackle this kind of people. You agreed to write only the (integer) division and mod (%) operations for him. Input Input  is  a  sequence  of  lines. Each  line  will contain an input number. One or more spaces. A  sign  ( division or  mod). Again  spaces. And  another  input  number.  Both the input numbers are non-negative  integer. Output A line for each input, each containing an integer. See the sample input and output. Output should not contain any extra space. INPUT 110 / 100 99 % 10 2147483647 / 2147483647 2147483646 % 2147483647 OUTPUT 1 9 1 2147483646
    83 - "ooooooooooooooh!
    If I could do the easy mathematics like my school days!!
    I can guarantee, that I'd not make any mistake this time!!"
    Says a smart university student!!
    But his teacher even smarter "Ok! I'd assign you such projects in your software lab. Don't be so sad."
    "Really!!" the  students  feels  happy. And  he  feels  so happy that he cannot see the smile in his teacher's face. The  rst project for the poor student was to make a calculator that can just perform the basic arithmetic operations. But like many other university students he doesn't like to do any project by himself. He just wants to collect programs from here and there. As you are a friend of him, he asks you to write the program. But, you are also intelligent enough to tackle this kind of people. You agreed to write only the (integer) division and mod (%) operations for him.
    Input
    Input  is  a  sequence  of  lines. Each  line  will contain an input number. One or more spaces. A  sign  ( division or  mod). Again  spaces. And  another  input  number.  Both the input numbers are non-negative  integer.
    Output
    A line for each input, each containing an integer. See the sample input and output. Output should not contain any extra space.
    INPUT
    110 / 100
    99 % 10
    2147483647 / 2147483647
    2147483646 % 2147483647
    OUTPUT
    1
    9
    1
    2147483646

---
+++ Beginner for kids/SET 9/divide.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class divide {
+   public static void main(String[]args){
+	  int a,b,c,d;
+	  int i,j;
+	  Scanner sc=new Scanner(System.in);
+	  a=sc.nextInt();
+	  b=sc.nextInt();
+	  c=b/a;
+	  d=b%a;
+	  System.out.println(c);
+	  System.out.println(d);
+		
+	  }}
+ 

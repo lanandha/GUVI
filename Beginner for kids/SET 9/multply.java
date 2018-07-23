a3a7482a24a9629d923d65786f32a99d6e283476
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-23 12:17:53 +0530

    My-Submission : Not Submitted 82 - Given length L and breadth B of a farm, print the area of the farm upto 5 decimal decimals(print the answer upto 5 decimal places). Sample Testcase : INPUT 1.626 2.31 OUTPUT 3.75606
    My-Submission : Not Submitted
    82 - Given length L and breadth B of a farm, print the area of the farm upto 5 decimal decimals(print the answer upto 5 decimal places).
    Sample Testcase :
    INPUT
    1.626 2.31
    OUTPUT
    3.75606

---
+++ Beginner for kids/SET 9/multply.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class multply {
+   public static void main(String[]args){
+	  int a,b,c;
+	  int i,j;
+	  Scanner sc=new Scanner(System.in);
+	  a=sc.nextInt();
+	  b=sc.nextInt();
+	  c=b*a;
+	  System.out.println(c);
+	
+	  }}
+ 

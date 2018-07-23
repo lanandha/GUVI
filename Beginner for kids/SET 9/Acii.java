8bf52287ebef4090f182648c04ab10606471304c
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-23 12:26:49 +0530

    84 - Given a character Ch, print its ASCII value. Sample Testcase : INPUT a OUTPUT 97
    84 - Given a character Ch, print its ASCII value.
    Sample Testcase :
    INPUT
    a
    OUTPUT
    97

---
+++ Beginner for kids/SET 9/Acii.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Acii {
+   public static void main(String[]args){
+	 char c;
+	 Scanner sc=new Scanner(System.in);
+	 c=sc.next().charAt(0);
+	 int a=(char)c;
+	 System.out.println(a);
+   }
+}
+ 

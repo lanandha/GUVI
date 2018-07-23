faee894d117f7f4d715237333f5b09d936c25de8
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-23 09:43:36 +0530

    78 - Given a number N, find whether it is a multiple of 13. Sample Testcase : INPUT 26 OUTPUT yes
    78 - Given a number N, find whether it is a multiple of 13.
    Sample Testcase :
    INPUT
    26
    OUTPUT
    yes

---
+++ Beginner for kids/SET 8/Multiply.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Multiply {
+public static void main(String[]args){
+	int a;
+	Scanner sc=new Scanner(System.in);
+	a=sc.nextInt();
+	if(a%13==0){
+		System.out.println("yes");
+	}else{
+		System.out.println("no");
+	}
+}
+}

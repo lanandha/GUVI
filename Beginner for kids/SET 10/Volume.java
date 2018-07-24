05b08100043bb504a5108ce3f048571d8f913317
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-24 15:30:19 +0530

    How to submit answer in codekata?(Video tutorial) 91 - Write a program to calculate the total surface area and volume of cuboid. Input contains three space separated positive integers L, B, H denoting the length, width and height of cuboid respectively. Sample Testcase : INPUT 1 2 3 OUTPUT 22 6
    How to submit answer in codekata?(Video tutorial)
    91 - Write a program to calculate the total surface area and volume of cuboid. Input contains three space separated positive integers L, B, H denoting the length, width and height of cuboid respectively.
    Sample Testcase :
    INPUT
    1 2 3
    OUTPUT
    22 6

---
+++ Beginner for kids/SET 10/Volume.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Volume {
+  public static void main(String[]args){
+	  int a,b,c,d,e,f;
+	  Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	 b=sc.nextInt();
+	 c=sc.nextInt();
+	 d=a*b*c;
+	 e=(2*a*b)+(2*b*c)+(2*c*a);
+System.out.print(e);
+System.out.print(" ");
+	 System.out.print(d);
+	 
+  }
+}

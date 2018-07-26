a45ddd9c2be34ca9bd59f34400309ebc28576e35
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-26 15:43:37 +0530

    96 - Given 3 numbers A,B,C. Find the sum of AP with a=A, d=B and n=C Sample Testcase : INPUT 1 1 2 OUTPUT 3
    96 - Given 3 numbers A,B,C. Find the sum of AP with a=A, d=B and n=C
    Sample Testcase :
    INPUT
    1 1 2
    OUTPUT
    3

---
+++ Beginner for kids/SET 10/ABC.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ABC {
+  public static void main(String[]args){
+	 int a,b,c;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	 b=sc.nextInt();
+	 c=sc.nextInt();
+	 int d=(c/2)*((2*a)+(c-1)*b);
+	 System.out.println(d);
+  }
+}

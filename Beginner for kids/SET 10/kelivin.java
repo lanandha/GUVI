17fb35d7dfc2afa8ce0017b9f3be532624414e93
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-26 15:22:15 +0530

    93 - Given a temperature in Celsius, convert and print in Kelvin. Sample Testcase : INPUT 21 OUTPUT 294
    93 - Given a temperature in Celsius, convert and print in Kelvin.
    Sample Testcase :
    INPUT
    21
    OUTPUT
    294

---
+++ Beginner for kids/SET 10/kelivin.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class kelivin {
+  public static void main(String[]args){
+	 int a;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	 System.out.println(a+273);
+  }
+}

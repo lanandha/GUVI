f88e4b9092901b54c061c1303ae458f0991acd3a
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-27 14:44:16 +0530

    2 - Given a number N, find its factorial. Input Size  : N <= 20 Sample Testcase : INPUT 5 OUTPUT 120
    2 - Given a number N, find its factorial.
    Input Size  : N <= 20
    Sample Testcase :
    INPUT
    5
    OUTPUT
    120

---
+++ player/SET 1/Factorial.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Factorial {
+ public static void main(String[]args){
+	 int a,i,c=1;
+	 Scanner sc= new Scanner(System.in);
+	 a=sc.nextInt();
+	 for(i=1;i<=a;i++){
+		 c=c*i;}
+		 System.out.println(c);
+	 
+ }
+}

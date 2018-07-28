472788070b9a5cecb740fa2fb94ef8111bc9c58e
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-28 17:12:40 +0530

    10 - Given 2 strings check whether they differ by only one character. Input Size : |s| <= 100000(complexity O(nlogn) or O(n)) Sample Testcase : INPUT aab aay OUTPUT yes
    10 - Given 2 strings check whether they differ by only one character.
    Input Size : |s| <= 100000(complexity O(nlogn) or O(n))
    Sample Testcase :
    INPUT
    aab aay
    OUTPUT
    yes

---
+++ player/SET 1/Count2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Count2 {
+  public static void main(String[]args){
+	  int i,j,c=0,c1=0;
+	  
+	  String s,sa;
+	  Scanner sc=new Scanner(System.in);
+	  s=sc.nextLine();
+	  char ch[]=s.toCharArray();
+int o=ch.length;
+	
+	  sa=sc.nextLine();
+	  char[] cc=sa.toCharArray();
+	  int n=cc.length;
+	
+	  for(i=0;i<o;i++){
+		  for(j=i+1;j<o;j++){
+			  if(ch[i]==ch[j]){
+				 c++; 
+				 break;
+			  }
+		  }
+	  }for(i=0;i<n;i++){
+		  for(j=i+1;j<n;j++){
+			  if(cc[i]==cc[j]){
+				 c1++; 
+				 break;
+			  }
+		  }
+	  }
+	  if(c>0 && c1>0){
+		  System.out.println("Yes");
+	  }else{
+		  System.out.println("no");
+	  }
+  }
+}

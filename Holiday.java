64c50c7fd7e9dbc239d51f4fe53956cf2558d700
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-30 14:19:03 +0530

    11 - Given a day, print if it is a holiday. Sample Testcase : INPUT Saturday Monday OUTPUT yes no
    11 - Given a day, print if it is a holiday.
    Sample Testcase :
    INPUT
    Saturday
    Monday
    OUTPUT
    yes
    no

---
+++ Holiday.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Holiday {
+  public static void main(String[]args){
+	  String a,d,c,b;
+	  Scanner sc=new Scanner(System.in);
+  a=sc.nextLine();
+  b=sc.nextLine();
+  c=a.substring(0,1);
+  d=b.substring(0,1);
+  if("s"==c){
+	  System.out.println("yes");
+  }else{
+	  System.out.println("No");
+  }if("s"==d){
+	  System.out.println("yes");
+  }else{
+	  System.out.println("No");
+  }
+  }
+  
+}

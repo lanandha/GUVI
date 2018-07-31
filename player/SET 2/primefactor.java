98354719c16dc02d9f6a147b83acd543885652b6
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-31 14:28:50 +0530

    19 - Given a number N, print its prime factors in sorted order. Input Size : 2 <= N <= 100000 Sample Testcase : INPUT 18 OUTPUT 2 3
    19 - Given a number N, print its prime factors in sorted order.
    Input Size : 2 <= N <= 100000
    Sample Testcase :
    INPUT
    18
    OUTPUT
    2 3

--- player/SET 2/primefactor.java
+++ player/SET 2/primefactor.java
@@ -1,4 +1,20 @@
-
+import java.util.*;
 public class primefactor {
-
+ public static void main(String[]args){
+	 Scanner in=new Scanner(System.in);
+	 int n,temp=0,a=0,b=0,c=0;
+	 n=in.nextInt();
+	 for(int i=2;i<n;i++){
+	 if(n%i==0){
+	 	for(int j=2;j<i;j++){
+	 		b=i%j;
+	 		if(b==0 && i!=2){
+	 			break;
+	 		}
+	 	}if(b==1 ||i==2){
+	 		System.out.print(i+" ");
+	 	}
+	 }
+	 	}
+ }
 }

05f786fa298de0988707a20664af824560f08071
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-31 09:50:56 +0530

    13 - Given a number N, print the sum of the squares of its digits. Input Size : 1 <= N <= 1000000000000000000 Sample Testcase : INPUT 19 OUTPUT 82
    13 - Given a number N, print the sum of the squares of its digits.
    Input Size : 1 <= N <= 1000000000000000000
    Sample Testcase :
    INPUT
    19
    OUTPUT
    82

---
+++ player/SET 2/Square.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Square {
+  public static void main(String[]args){
+	  int n,r,sum=0;
+	  Scanner sc=new Scanner(System.in);
+	  n=sc.nextInt();
+	  while(n!=0){
+		  r=n%10;
+		  sum=sum+(r*r);
+		  n=n/10;
+	  }
+	  System.out.println(sum);
+  }
+}

d2ca9c9a526687e7e122f230b276bf53ae2b3bc3
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-31 13:56:34 +0530

    17 - Given two numbers L,R print the smallest number which is divisible by both L and R. Input Size : 1 <= L,R <= 100000 Sample Testcase : INPUT 10 130 OUTPUT 130
    17 - Given two numbers L,R print the smallest number which is divisible by both L and R.
    Input Size : 1 <= L,R <= 100000
    Sample Testcase :
    INPUT
    10 130
    OUTPUT
    130

--- Factorial.java
+++ Factorial.java
@@ -1,11 +1,12 @@
 import java.util.*;
 public class Factorial {
-public static void main(String[]args){
-	int a,fact=1;
-	Scanner sc=new Scanner(System.in);
-	a=sc.nextInt();
-	for(int i=1;i<=a;i++){
-		fact=fact*i;
-	}System.out.println(fact);
-}
+ public static void main(String[]args){
+	 int a,i,c=1;
+	 Scanner sc= new Scanner(System.in);
+	 a=sc.nextInt();
+	 for(i=1;i<=a;i++){
+		 c=c*i;}
+		 System.out.println(c);
+	 
+ }
 }

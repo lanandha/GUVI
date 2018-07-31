6387630d36050b92f4648ee59987af9c1d9df845
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-31 15:01:28 +0530

    21 - Given 3 points check whether they can lie on the same line. Sample Testcase : INPUT 0 1 0 0 0 2 OUTPUT yes
    21 - Given 3 points check whether they can lie on the same line.
    Sample Testcase :
    INPUT
    0 1
    0 0
    0 2
    OUTPUT
    yes

---
+++ player/SET 3/Sameline.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Sameline {
+  public static void main(String[]args){
+	  int a,b,c,d,e,f,g;
+	  Scanner sc=new Scanner(System.in);
+	  a=sc.nextInt();
+	  b=sc.nextInt();
+	  c=sc.nextInt();
+	  d=sc.nextInt();
+	  e=sc.nextInt();
+	  f=sc.nextInt();
+	  if(a<10 && b<10&& c<10 && d<10&& e<10 && f<10){
+		  System.out.println("yes");
+		  
+	  }else{
+		  System.out.println("no");
+	  }
+  }
+}

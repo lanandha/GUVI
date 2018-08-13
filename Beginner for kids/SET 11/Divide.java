68bd9477a7882727e518efbb9683bab17db2cee9
lanandha <41110722+lanandha@users.noreply.github.com>
2018-08-13 09:40:42 +0530

    102 - Given a number N, print the number obtained by dividing it by 2 recursively till it is not divisible. Input Size : 1 <= N <= 10 Sample Testcase : INPUT 10 3 OUTPUT 5 3
    102 - Given a number N, print the number obtained by dividing it by 2 recursively till it is not divisible.
    Input Size : 1 <= N <= 10
    Sample Testcase :
    INPUT
    10
    3
    OUTPUT
    5
    3

---
+++ Beginner for kids/SET 11/Divide.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Divide {
+ public static void main(String[]args){
+	 int a;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	 while(a%2==0){
+		 a=a/2;
+	 }
+	 System.out.println(a);
+	 
+ }
+}

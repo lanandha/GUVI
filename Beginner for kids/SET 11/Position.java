4eebe67ba00c431957ab8d2458138b376d81b2f3
lanandha <41110722+lanandha@users.noreply.github.com>
2018-08-13 10:17:15 +0530

    106 - Given 2 numbers N and K followed by N numbers print the Kth odd number. Sample Testcase : INPUT 4 2 1 2 3 4 OUTPUT 3
    106 - Given 2 numbers N and K followed by N numbers print the Kth odd number.
    Sample Testcase :
    INPUT
    4 2
    1 2 3 4
    OUTPUT
    3

---
+++ Beginner for kids/SET 11/Position.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Position {
+ public static void main(String[]args){
+	 int a,b;
+	 int i;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	 b=sc.nextInt();
+	 int arr[]=new int[a];
+	 for(i=0;i<a;i++){
+		 arr[i]=sc.nextInt();
+	 }
+	// for(i=b;i<a;i++){
+		 System.out.println(arr[b]);
+	 //}
+	 
+ }
+}

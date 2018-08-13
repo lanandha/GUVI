7621d9247d54f47fae0d758ec0a8356ac8db8d39
lanandha <41110722+lanandha@users.noreply.github.com>
2018-08-13 10:34:00 +0530

    107 - Given 3 Numbers A,B,C. Print (A*B)/C. Input Size : |s| <= 1000 Sample Testcase : INPUT 1 2 3 OUTPUT 0
    107 - Given 3 Numbers A,B,C. Print (A*B)/C.
    Input Size : |s| <= 1000
    Sample Testcase :
    INPUT
    1 2 3
    OUTPUT
    0

---
+++ Beginner for kids/SET 11/Multi.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Multi {
+ public static void main(String[]args){
+	 int a,b,c;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	 b=sc.nextInt();
+	 c=sc.nextInt();
+	 System.out.println((a*b)/c);
+ }
+}

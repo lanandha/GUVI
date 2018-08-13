3159854f623d50612ff0ced451dc51f762caab88
lanandha <41110722+lanandha@users.noreply.github.com>
2018-08-13 10:38:33 +0530

    108 - Given 2 numbers N and K followed by N numbers print the Kth smallest number. Sample Testcase : INPUT 4 2 1 2 3 4 OUTPUT 2
    108 - Given 2 numbers N and K followed by N numbers print the Kth smallest number.
    Sample Testcase :
    INPUT
    4 2
    1 2 3 4
    OUTPUT
    2

---
+++ Beginner for kids/SET 11/PositionLength.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class PositionLength {
+ public static void main(String[]args){
+	 int a,b;
+	 int i;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	 b=sc.nextInt();
+	 int arr[]=new int[a];
+	 for(i=0;i<a;i++){
+		 arr[i]=sc.nextInt();
+	 }
+		 System.out.println(arr[b-1]);
+	 
+ }
+}
